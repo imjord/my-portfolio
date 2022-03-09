f9fd750bb2382d6737659a156190d249960f1311
imjord <dean.jay.goat@gmail.com>
2022-03-09 11:13:35 -0700

    fixed images

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ var projectsObj = [
     info: "A simple budget tracker that works offline PWA",
   },
   {
-    src: "./assets/csharpstaff.png",
+    src: "./assets/csharpstaff.PNG",
     name: "C# Console Staff App",
     skill: "C#",
     info: "A C# console app for staff team members",
   },
   {
-    src: "./assets/expo.png",
+    src: "./assets/expo.PNG",
     name: "Mobile Image Share App EXPO",
     skill: "Javascript, Expo, React-Native",
     info: "A simple app made for your mobile device to share images from your photos",
@@ -43,7 +43,7 @@ var projectsObj = [
     info: "A group project that I was the sole backend developer for. Created a site to like resturants",
   },
   {
-    src: "./assets/games.png",
+    src: "./assets/games.PNG",
     name: "MVC Game database",
     skill: "Javascript, EJS, NodeJS, MongoDB",
     info: "A game database that you can add your games to.",
@@ -55,7 +55,7 @@ var projectsObj = [
     info: "A simple guessing game made with java. Has a GUI",
   },
   {
-    src: "./assets/mysql.png",
+    src: "./assets/mysql.PNG",
     name: "mysql Voting App",
     skill: "Javascript, NodeJS, mySQL",
     info: "A voting backend database made with mysql and express",
@@ -67,7 +67,7 @@ var projectsObj = [
     info: "A note taker app made with CRUD using express",
   },
   {
-    src: "./assets/port.png",
+    src: "./assets/port.PNG",
     name: "React Portfolio",
     skill: "Javascript, React",
     info: "My portfolio. but in react",
@@ -79,25 +79,25 @@ var projectsObj = [
     info: "A quiz made in javascript",
   },
   {
-    src: "./assets/quote.png",
+    src: "./assets/quote.PNG",
     name: "Quote Generator",
     skill: "Javascript, HTML, CSS",
     info: "Quote Generator using an api to generate quotes. Can also tweet them",
   },
   {
-    src: "./assets/rpg-node.png",
+    src: "./assets/rpg-node.PNG",
     name: "Node RPG",
     skill: "Javascript, NodeJS, OOP",
     info: "Using javascript OOP to make a rpg",
   },
   {
-    src: "./assets/run.png",
+    src: "./assets/run.PNG",
     name: "run buddy",
     skill: "HTML, CSS",
     info: "A website for a running company",
   },
   {
-    src: "./assets/scroll.png",
+    src: "./assets/scroll.PNG",
     name: "infinite scroll",
     skill: "Html, css, javascript",
     info: "Able to infinitly scroll through unsplash photos api",
@@ -109,13 +109,13 @@ var projectsObj = [
     info: "An ecommerce website to buy clothing. made the backend for this group project",
   },
   {
-    src: "./assets/weather.png",
+    src: "./assets/weather.PNG",
     name: "Weather Site",
     skill: "HTML, CSS, Javascript",
     info: "A website made with openweather api",
   },
   {
-    src: "./assets/work.png",
+    src: "./assets/work.PNG",
     name: "Workscheduler",
     skill: "HTML, CSS,Javascript, Moment.js",
     info: "A website for making a busy schedule",
